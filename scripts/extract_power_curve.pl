34f90f9172f9c1c5040ed3020bbac5bf37fcc3f8
othocaeS <xaw5719@wmich.edu>
2016-06-16 15:44:27 -0400

    still figuring out best way to script plotting

--- scripts/extract_power_curve.pl
+++ scripts/extract_power_curve.pl
@@ -1,4 +1,17 @@
 #!/usr/local/bin/perl
 
-use PDL::Graphics::Gnuplot;
-gplot( xvals(5)**2 );
+use Graphics::GnuplotIF;
+my $plot = Graphics::GnuplotIF->new (
+    style        => 'histogram', # one of the gnuplot line     styles (see below)
+    title        => "$mytitle",      # string
+    xlabel       => '$myxlabel',     # string
+    ylabel       => '$myylabel',     # string
+    xrange       => [@myxrange],      # array reference; autoscaling, if empty
+    yrange       => [@myyrange],      # array reference; autoscaling, if empty
+    plot_titles  => [ "$titles" ],      # array of strings; titles used in the legend
+    persist      => 0,       # let plot windows survive after gnuplot exits
+                             # 0 : close / 1 : survive
+    );
+
+    $plot_glacier->gnuplot_plot_xy ( \@x, \@y );
+    $plot_glacier->gnuplot_pause();
