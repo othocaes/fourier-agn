4a22731cdad534644bce2b1e43f218aced7e2b6f
caes <xaw5719@wmich.edu>
2016-07-22 17:58:25 -0400

    set up new data naming convention

--- scripts/analyze_lightcurves.sh
+++ scripts/analyze_lightcurves.sh
@@ -1,26 +1,30 @@
 #!/usr/local/bin/bash
 
-for curvefile in data/STORM_III/lightcurves/Δt＝0.1/*
+timestep="0.1"
+lc_dir="data/STORM_III/lightcurves/Δt＝${timestep}"
+ref_band='HST(𝛌＝1367)'
+ref_curve="$lc_dir/${ref_band}.lc"
+error_type="2"
+# error types:
+#   0 for covariance matrix, 1 for likelihood function, 2 for monte carlo
+
+echo Using lightcurves in $lc_dir.
+
+for echo_curve in $lc_dir/*
 do 
-    echo Running psdlag for $curvefile.
-    newfilename="analyses/$(basename $curvefile|sed 's@\(.*\)\.lc@\1@')≻HST1367_$(basename $(dirname $curvefile))"
+    echo_band=$(basename $echo_curve|sed 's@\(.*\)\.lc@\1@')
+    echo -n $(date "+%R")\: Running psdlag for $echo_band against $ref_band.
+    newfilename="analyses/${echo_band}≻${ref_band}_Δt＝${timestep}"
     echo "2" > tmp.psdlagargs
-    echo "data/STORM_III/lightcurves/Δt＝0.1/HST1367.lc 0" >> tmp.psdlagargs
-    echo "$curvefile 0" >> tmp.psdlagargs
+    echo "$ref_curve 0" >> tmp.psdlagargs
+    echo "$echo_curve 0" >> tmp.psdlagargs
     echo "8 0.005 0.019 0.0425 0.07 0.11 0.17 0.24 0.4 0.603" >> tmp.psdlagargs
-    # covariance matrix errors
     echo "0" >> tmp.psdlagargs
-
-    # likelihood function errors
-    # echo "1" >> tmp.psdlagargs
-
-    # monte carlo errors
-    # echo "2" >> tmp.psdlagargs
-
     echo ".9999 .9999 .9999 0.49 0.42 0.30 0.36 1.2 -0.60 -0.89 -0.72 2.39 -0.74 -1.5 -1 -3.1 -1.3 -2.3 -2 -1.9 -1.6 -2.6 -2 -1.3 -1.8 -2.6 -1.2 0 -2 -2.7 -1.3 0.63" >> tmp.psdlagargs
     echo "0:0 0" >> tmp.psdlagargs
-    echo "0" >> tmp.psdlagargs
+    echo $error_type >> tmp.psdlagargs
     echo "0" >> tmp.psdlagargs
     echo "100 50 50 mcmc.dat" >> tmp.psdlagargs
-    time bin/psdlag tmp.psdlagargs >> $newfilenam e
+    time bin/psdlag tmp.psdlagargs >> $newfilename
+    echo ""
 done
--- scripts/prep_lcfiles.sh
+++ scripts/prep_lcfiles.sh
@@ -1,3 +1,11 @@
 #!/usr/local/bin/bash
+timestep="0.1"
+newdir="data/STORM_III/lightcurves/Δt＝${timestep}"
+mkdir -p $newdir
 
-for file in data/STORM_III/raw/separated/*; do scripts/heasoft_to_psdlag.pl < $file > data/STORM_III/lightcurves/Δt＝0.1/$(basename $file|sed 's@\(.*\)\.tab@\1.lc@'); echo Processed $file; done
+for file in data/STORM_III/raw/separated/*
+do
+    newfile=$(basename $file|sed 's@\(.*\)\.tab@\1.lc@')
+    scripts/heasoft_to_psdlag.pl < $file > ${newdir}/${newfile}
+    echo Processed $file
+done
